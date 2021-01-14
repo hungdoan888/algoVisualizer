c835b9de1c960e4aaad5fe6bf08c5d58290a448f
hungdoan888 <73352631+hungdoan888@users.noreply.github.com>
2021-01-13 22:33:34 -0800

    bubbleSort commit

--- bubbleSort/js/bubbleSort.js
+++ bubbleSort/js/bubbleSort.js
@@ -4,27 +4,83 @@
 
 const container = document.querySelector(".data-container");
 
+// ---------------
+// Random Blocks -
+// ---------------
+
+function randomBlocks() {
+  var num = parseInt(document.getElementById("random").value);
+  if (!Number.isInteger(num)) {
+    alert("Invalid Input: Integer Required");
+    return;
+  }
+
+  if (num < 1 | num > 100) {
+    alert("Invalid Input: Please select an integer bewtween 1 and 100");
+    return;
+  }
+
+  var array = [];
+  for (let i = 0; i < num; i++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+
+  generateBlocks(array);
+  changeLabelsToWhiteIfMoreThan20();
+}
+
+function changeLabelsToWhiteIfMoreThan20 () {
+  let blockLabel = document.querySelectorAll(".blockLabel");
+  if (blockLabel.length <= 20) {
+    return 
+  }
+
+  for (let i = 0; i < blockLabel.length; i++) {
+    blockLabel[i].style.color = "#FFF";
+  }
+}
+
+// -------
+// Array -
+// -------
+function arrayBlocks() {
+  let array = document.getElementById("array").value;
+  array = array.split(",")
+  for (let i = 0; i < array.length; i++) {
+    const value = parseInt(array[i])
+    if (isNaN(value)) {
+      alert("Invalid Array");
+      return
+    }
+    array[i] = value
+  }
+
+  if (array.length < 1 | array.length > 100) {
+    alert("Invalid Input: Array must have length between 1 and 100");
+    return;
+  }
+
+  generateBlocks(array);
+  changeLabelsToWhiteIfMoreThan20();
+}
+
+
 // -----------------
 // Generate Blocks -
 // -----------------
 
-function generateBlocks(num = 20) {
-
+function generateBlocks(array) {
   // Delete all nodes in container
-  deteNodesInContainer();
+  deleteNodesInContainer();
 
-  if (num && typeof num !== "number") {
-    alert("First argument must be a typeof Number");
-    return;
-  }
-
-  for (let i = 0; i < num; i++) {
-    const value = Math.floor(Math.random() * 100);
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i] / Math.max(...array) * 100;
+    console.log(Math.max(...array))
 
     // Block
     const block = document.createElement("div");
     block.classList.add("block");
-    block.style.width = 90 / num + "%";  // leave (100 - 90 = 10%) for margins
+    block.style.width = 100 / array.length + "%";
 
     // Color Bar
     const colorBar = document.createElement("div");
@@ -35,7 +91,7 @@ function generateBlocks(num = 20) {
     // Block Label
     const blockLabel = document.createElement("div");
     blockLabel.classList.add("blockLabel");
-    blockLabel.innerHTML = value;
+    blockLabel.innerHTML = array[i];
     block.appendChild(blockLabel);
 
     // Container
@@ -43,7 +99,7 @@ function generateBlocks(num = 20) {
   }
 }
 
-function deteNodesInContainer() {
+function deleteNodesInContainer() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
@@ -116,7 +172,8 @@ await changeCodeColorWithDelay("code10", delay)
 
 function changeCodeColor(id) {
   let codeLines = document.querySelectorAll(".codeSection");
-  for (let i = 0; i < 10; i++) {
+  let numLinesCode = document.getElementById("codeSection").childElementCount;
+  for (let i = 0; i < numLinesCode; i++) {
     document.getElementById("code" + i).style.backgroundColor  = "#f2f2f2";
     document.getElementById("code" + i).style.color = "#a6a6a6";
   }
@@ -138,10 +195,4 @@ function changeCodeColorWithDelay(id, delay) {
 function swap(el2, el1) {
     container.insertBefore(el2, el1);
 }
-
-// ------
-// Main -
-// ------
-
-document.getElementById("randomButton").onclick = generateBlocks();
 
